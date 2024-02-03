da0ca06fd599915d8e096a235f6cd39f5987722a
Theo Snelleman <theo@zolder.io>
2024-02-03 19:15:16 +0100

    Create initial package

---
+++ setup.py
@@ -0,0 +1,27 @@
+from setuptools import setup, find_packages
+
+VERSION = '0.0.1'
+DESCRIPTION = 'XSense Python Module'
+LONG_DESCRIPTION = 'Python module to interact with XSense Home Security'
+
+
+setup(
+    name="xsense",
+    version=VERSION,
+    author="Theo Snelleman",
+    author_email="<theo@snelleman.net>",
+    description=DESCRIPTION,
+    long_description=LONG_DESCRIPTION,
+    packages=find_packages(),
+    install_requires=[],
+
+    keywords=['python', 'xsense'],
+    classifiers=[
+        "Development Status :: 3 - Alpha",
+        "Intended Audience :: Education",
+        "Programming Language :: Python :: 2",
+        "Programming Language :: Python :: 3",
+        "Operating System :: MacOS :: MacOS X",
+        "Operating System :: Microsoft :: Windows",
+    ]
+)
