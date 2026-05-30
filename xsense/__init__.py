5fea9fd3646aac677854e77ac29d201178f7734a
Wheemer <wheemer@hotmail.com>
2026-05-30 00:29:33 -0230

    Add MQTT live update helpers

--- xsense/__init__.py
+++ xsense/__init__.py
@@ -5,4 +5,12 @@ from .house import House
 from .station import Station
 from .device import Device
 
-from .mqtt_helper import MQTTHelper
+from .mqtt_helper import (
+    MQTTHelper,
+    house_event_topic,
+    parse_message_payload,
+    presence_topic,
+    shadow_update_topic,
+    shadow_wildcard_topic,
+    should_ignore_shadow_topic,
+)
